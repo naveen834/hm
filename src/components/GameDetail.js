e4e51e4b3df2c35061f3a45d69e01cbd64b52812
naveen <naveen.kumar.met14@itbhu.ac.in>
2021-02-28 19:51:44 +0530

    New

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,21 +1,21 @@
-import { motion } from 'framer-motion';
 import React from 'react';
+//Styling and Animation
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 //Redux
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-//Styling and Animation
-import styled from 'styled-components';
-import apple from '../img/apple.svg';
-import gamepad from '../img/gamepad.svg';
-import nintendo from '../img/nintendo.svg';
+import { smallImage } from '../util';
 //IMAGES
 import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
 //Star Images
 import starEmpty from '../img/star-empty.png';
 import starFull from '../img/star-full.png';
-import steam from '../img/steam.svg';
-import xbox from '../img/xbox.svg';
-import { smallImage } from '../util';
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
 
@@ -58,6 +58,7 @@ const GameDetail = ({ pathId }) => {
         return gamepad;
     }
   };
+
   //Data
   const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
@@ -135,15 +136,12 @@ const CardShadow = styled(motion.div)`
   top: 0;
   left: 0;
   z-index: 5;
-
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
-
   &::-webkit-scrollbar-thumb {
     background-color: #ff7676;
   }
-
   &::-webkit-scrollbar-track {
     background: white;
   }
